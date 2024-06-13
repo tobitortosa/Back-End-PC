074043ab4c65f9d20d23a94cb2c57229e0e36f25
tobitortosa <tobiastortosa17@gmail.com>
2024-06-13 19:39:10 -0300

    fix

--- src/db.js
+++ src/db.js
@@ -1,16 +1,21 @@
 require("dotenv").config();
-const { Sequelize } = require("sequelize");
 const fs = require("fs");
 const path = require("path");
-const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_PORT } = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
 
-const sequelize = new Sequelize(
-  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`,
-  {
-    logging: false,
-    native: false,
-  }
-);
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize('postgres://' + DB_USER + ":" +  DB_PASSWORD + "@" + DB_HOST + ":5432/" + DB_NAME, {
+host: DB_HOST,
+dialect: DB_USER,
+logging: false,
+pool: {
+    min: 0,
+    max: 5,
+    idle: 1000
+     }
+});
+
 const basename = path.basename(__filename);
 
 const modelDefiners = [];
